63ce69d79260c3c695ac9076ba8810b991dcfba4
Kaina Terto <kterto@capyba.com>
2019-10-06 13:11:51 -0300

    react_hooks_on_market_place

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -37,7 +37,7 @@ export default function Home() {
   }, []);
 
   function handleAddProduct(id) {
-    dispatch.apply(CartActions.addToCartRequest(id));
+    dispatch(CartActions.addToCartRequest(id));
   }
 
   return (
