e0339f849d83f35738e40a3572040da29c86d212
Kaina Terto <kterto@capyba.com>
2019-10-13 20:36:02 -0300

    start

--- modulo09/src/App.js
+++ modulo09/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <div className="App" />
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
