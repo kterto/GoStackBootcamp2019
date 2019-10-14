cd9ec6e79723518bc8e73c9508e6f028c66f966b
Kaina Terto <kterto@capyba.com>
2019-10-14 14:04:41 -0300

    gobarber web half-way

--- modulo09/.eslintrc.js
+++ modulo09/.eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   plugins: ['react', 'jsx-a11y', 'import', 'react-hooks', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'react/jsx-props-no-spreading': 0,
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
@@ -33,12 +34,12 @@ module.exports = {
     'no-console': ['error', { allow: ['tron'] }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src'
+      }
+    }
   }
-  // settings: {
-  //   'import/resolver': {
-  //     'babel-plugin-root-import': {
-  //       rootPathSuffix: 'src'
-  //     }
-  //   }
-  // }
 };
